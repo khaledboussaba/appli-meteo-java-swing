1b73431039266ac9c41df38293fa943379090021
khaled <khaledboussaba@gmail.com>
2020-04-20 13:52:49 +0200

    first get request using okhttp and show the data in the console

---
+++ projectSteps.md
@@ -0,0 +1,5 @@
+=> create a java project
+=> create two classes (MainFraim & Application)
+=> add OkHttp and okio jars to the project (dont forget to add them to build path)
+=> connect to darksky.net and get the api key (it's personal)
+=> use include the api key in the URL with latitude and longitude to get the data from darksky
--- src/fr/dev/weather/MainFrame.java
+++ src/fr/dev/weather/MainFrame.java
@@ -1,11 +1,33 @@
 package fr.dev.weather;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import javax.swing.JFrame;
+import java.io.IOException;
 
 public class MainFrame extends JFrame {
 
     public MainFrame(String title) {
         super(title);
+
+        String apiKey = "dcac8953d6a57d38c6d818c7d86e2589";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            System.err.println("ERROR : " + e);
+        }
+
     }
 
 }
