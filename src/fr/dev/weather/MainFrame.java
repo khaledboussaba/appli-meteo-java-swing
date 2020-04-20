3c4811e2b8634f25f1e8a818fbc9cf7302381cb1
khaled <khaledboussaba@gmail.com>
2020-04-20 14:28:02 +0200

    better way to use okhttp requesting: use SwingWorker to make the request asynchronous

--- src/fr/dev/weather/MainFrame.java
+++ src/fr/dev/weather/MainFrame.java
@@ -5,8 +5,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class MainFrame extends JFrame {
 
@@ -18,16 +19,34 @@ public class MainFrame extends JFrame {
         double longitude = -122.4233;
         String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(forecastUrl).build();
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            System.out.println(response.body().string());
-        } catch (IOException e) {
-            System.err.println("ERROR : " + e);
-        }
+        System.out.println("Before request ...");
+        new SwingWorker<String, Void>() {
 
+            @Override
+            protected String doInBackground() throws Exception {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder().url(forecastUrl).build();
+                Call call = client.newCall(request);
+                try {
+                    Response response = call.execute();
+                    return response.body().string();
+                } catch (IOException e) {
+                    System.err.println("ERROR : " + e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    System.out.println(get()); // get() method return the return of doInBackGround methed
+                } catch (InterruptedException | ExecutionException e) {
+                    System.err.println("ERROR : " + e);
+                }
+            }
+
+        }.execute();
+        System.out.println("After request ...");
     }
 
 }
